71db632dca6df19c8d4f48184906550fc25fc8ee
The Ghost in the Machine <nitrogen1818@gmail.com>
2019-02-14 21:42:16 -0500

    bare window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use tcod::RootConsole as Root;
+use tcod::Console;
+use tcod::input::KeyCode;
+use tcod::BackgroundFlag;
+
+/// The width of the map display area
+pub const MAP_WIDTH: i32 = 80;
+/// The height of the map display area
+pub const MAP_HEIGHT: i32 = 50;
+
 fn main() {
-    println!("Hello, world!");
+    use self::KeyCode::*;
+
+    let mut console = Root::initializer()
+        .size(MAP_WIDTH, MAP_HEIGHT)
+        .title("Monochrome Rogue-like: The Original Masterpiece")
+        .fullscreen(false)
+        .init();
+
+    let mut exit = false;
+    while !(console.window_closed() || exit) {
+        console.clear();
+        console.put_char(40, 25, '@', BackgroundFlag::Set);
+        console.flush();
+        let keypress = console.wait_for_keypress(true);
+        match keypress.code {
+            Escape => exit = true,
+            _ => ()
+        }
+    }
 }
